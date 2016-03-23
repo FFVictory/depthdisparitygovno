73a64c9d9837ca0ffdae7873569795b66a610ad3
Alex Kudrin <aleksejs.kudrins@gmail.com>
2016-03-23 13:37:27 +0000

    added gitImage function

---
+++ KODELO/getImage.m
@@ -0,0 +1,8 @@
+function image = getImage( path )
+%UNTITLED Summary of this function goes here
+%   Detailed planation goes here
+    
+   image = imread(path);
+     
+end
+
--- KODELO/main.m
+++ KODELO/main.m
@@ -1,7 +1,11 @@
-function [ output_args ] = untitled( input_args )
+function disparity = main( ~ )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-
+    leftImage = getImage('../testimages/testL.jpg');
+    rightImage = getImage('../testimages/testR.jpg');
+    
+    imshow(leftImage);
+    
 end
 
