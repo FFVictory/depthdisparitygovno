9e9bc40f73d89b7b6ae5625a36bafcf73c11b63d
Alex Kudrin <aleksejs.kudrins@gmail.com>
2016-03-26 18:11:21 +0000

    fixing bugs

--- KODELO/main.m
+++ KODELO/main.m
@@ -20,15 +20,16 @@ function disparity = main( ~ )
             leftImageX = (x*8)+1;
             leftImageY = (y*8)+1;
             
-
             leftImageWindow = getImageWindow(leftImageX, leftImageY,imageWindowLength,imageWindowWidth,leftImage);
             
                 imageSSDArray = getImageSSDArray(rightImage, imageLength, imageWidth, leftImageWindow, imageWindowLength, imageWindowWidth);
                 
                 [M , I] = min(reshape(imageSSDArray, numel(imageSSDArray) , 1));
-                [minSSDX,minSSDY] = ind2sub(size(imageSSDArray), I);
+                [minSSDY, minSSDX] = ind2sub(size(imageSSDArray), I);
+                
                 rightWindowSSDX = ((minSSDX-1)*8)+1;
                 rightWindowSSDY = ((minSSDY-1)*8)+1;
+                
                 rightImageWindow = getImageWindow(rightWindowSSDX,rightWindowSSDY,imageWindowLength,imageWindowWidth,rightImage);
                 
                 pixelSSDArray = [];
@@ -40,7 +41,7 @@ function disparity = main( ~ )
                         pixelSSDArray = getPixelSSDArray(leftPixel, imageWindowLength, imageWindowWidth, rightImageWindow);
                        
                         [minWindowSSD , MinimumSSD] = min(reshape(pixelSSDArray, numel(pixelSSDArray) , 1));
-                        [pixelSSDX,pixelSSDY] = ind2sub(size(pixelSSDArray), MinimumSSD);
+                        [pixelSSDY,pixelSSDX] = ind2sub(size(pixelSSDArray), MinimumSSD);
                         
                         RightPixelMinSSDX = rightWindowSSDX + pixelSSDX;
                         RightPixelMinSSDY = rightWindowSSDY + pixelSSDY;
@@ -50,12 +51,14 @@ function disparity = main( ~ )
                         
                         DisparityValue = sqrt((LeftPixelX - RightPixelMinSSDX)^2) + sqrt((LeftPixelY - RightPixelMinSSDY)^2);
                         
-                        disparityMap(LeftPixelX , LeftPixelY) = DisparityValue;
+                        disparityMap(LeftPixelY , LeftPixelX) = DisparityValue;
 
                      end
                 end 
                 
         end
     end
+    
+    imshow(rightImage);
     imshow(disparityMap);
 end
